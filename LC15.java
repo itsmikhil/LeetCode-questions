ea43dd7210291767c507c1a3a8f3cbd5385a13a0
itsmikhil <ailani.mikhil@gmail.com>
2026-05-18 15:12:52 +0530

    LC15

--- LC15.java
+++ LC15.java
@@ -31,3 +31,49 @@ class Solution {
         return list;
     }
 }
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // optimal
+        // pichle mai hum set use karte the to avoid duplicates 
+        // yaha pe hum usko eleiminate kar rahe hai
+        // isliye we will use sorted array directly
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        // we will use 3 pointer approach
+        // i->first
+        // j->second (initially i+1)
+        // k->third (initially nums.length-1)
+        // if sum greater then k--
+        // if sum is smaller then j++
+        for(int i=0;i<nums.length;i++){
+            // i wala element ko same rakehenge toh pichla wala case fir se same hoga
+            // because j,k are dependent on i
+            // teeno milke he sum dete hai
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                    k--;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    List<Integer> triplet=new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]);
+                    ans.add(triplet);
+                    // because agar galti se j aur k same el ke duplicate ke upaar rehe gaye toh duplicate triplet milega
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans; 
+    }
+}

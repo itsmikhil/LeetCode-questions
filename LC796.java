6787d1ff44b12b7f4377318dac9d472b04409a61
itsmikhil <ailani.mikhil@gmail.com>
2026-05-18 11:46:42 +0530

    LC796

--- LC796.java
+++ LC796.java
@@ -11,3 +11,19 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        // we will join original string twice
+        // rotationrotation subtring nikalte raho original string ke length ki
+        // mil jayega turant
+        String check=s+s;
+        for(int i=0;i<s.length();i++){
+            String curr=check.substring(i,i+s.length());
+            if(curr.equals(goal)){
+                return true;
+            }
+        }
+        return false;
+    }
+}

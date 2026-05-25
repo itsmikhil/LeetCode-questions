490551ecdcf587a7de394c860ca1f3f64e1353f1
itsmikhil <ailani.mikhil@gmail.com>
2026-05-25 16:49:16 +0530

    LC735- comments updated

--- LC735.java
+++ LC735.java
@@ -1,8 +1,13 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         // important
+
+        // INTUITION:- neg el humare previously iterated element ko destroy kar raha hai 
+        // means we get the need of LIFO structure thats why STACK
+
+        // Approach
         // jaise he postive dikhe hum stack mai dalte hai
-        // jaise he nagative value dikhe hum destry karte hai based on value
+        // jaise he nagative value dikhe hum destroy karte hai based on value
         // Case 1:-agar negative value aayi aur stack empty hai toh hum usse push karte hai kyuki woh  
         // left direction mai travel karega aur uss taraf koyi val baachi nhi hai isliye hum usse push kar rahe hai stack mai 
         // Case 2:- agar stack ke top negative hai aur curr negative val ko bhi hum push karenge because woh dono same
@@ -12,22 +17,28 @@ class Solution {
         
         Stack<Integer> s=new Stack<>();
         for(int i=0;i<asteroids.length;i++){
-            if(asteroids[i]>0){
-                s.push(asteroids[i]);
-            }else if(asteroids[i]<0){
-                while(!s.isEmpty() && s.peek()>0 && Math.abs(asteroids[i])>s.peek()){
-                    s.pop();
-                }
-                if(!s.isEmpty() && s.peek()>0 && s.peek()==Math.abs(asteroids[i])){
-                    s.pop();
-                    continue;
-                }
-                // important 
-                if(s.isEmpty() || s.peek()<0){
-                    System.out.println(asteroids[i]);
-                    s.push(asteroids[i]);
-                }
+            // posi dekhte he push karo
+           if(asteroids[i]>=0){
+            s.push(asteroids[i]);
+           }else{
+            // case of collision
+            // bigger neg destroying small posi els case
+            while(!s.isEmpty() && s.peek()>0 && Math.abs(asteroids[i])>s.peek()){
+                s.pop();
             }
+            // if s.peek() and neg el are equal then both get destroyed 
+            // thats why neg el shouldnt be pushed in stack thats why "continue"
+            if(!s.isEmpty() && s.peek()>0 && Math.abs(asteroids[i])==s.peek()){
+                s.pop();
+                continue;
+            }
+            // agar neg el is sabko destroy kar diya toh usse push karo stack mai
+            // ya phir stack ke andar bhi neg element ho toh daaldo 
+            // because woh dono fir same direction mai travel karte rahenge and they will travel happily
+            if(s.isEmpty() || s.peek()<0){
+                s.push(asteroids[i]);
+            }
+           }
         }
         int[] ans = new int[s.size()];
         int idx = s.size() - 1;

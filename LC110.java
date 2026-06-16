fc451210a9a7c9aaebb97d9e0a24c731e43866f3
itsmikhil <ailani.mikhil@gmail.com>
2026-06-16 11:38:49 +0530

    LC110

--- LC110.java
+++ LC110.java
@@ -1,28 +1,51 @@
 class Solution {
-    boolean result=true;
+    // Brute force
+    // o(n^2)
+    // for every node we are finding its height
     int height(TreeNode root){
         if(root==null){
             return 0;
         }
         int left=height(root.left);
-        if(left==-1){
-            return -1;
-        }
         int right=height(root.right);
-        if(right==-1){
-            return -1;
-        }
-        int dif=left>right?left-right:right-left;
-        if(dif>1){
-            result=false;
-            return -1;
-        }
         return Math.max(left,right)+1;
-        
     }
     public boolean isBalanced(TreeNode root) {
-        height(root);
-        return result;
-    
+        if(root==null){
+            return true;
+        }
+        int leftHeight=height(root.left);
+        int rightHeight=height(root.right);
+        if(Math.abs(leftHeight-rightHeight)>1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
+
+class Solution {
+    // Optimal
+    // Important samjh lo
+    // ek he baar height find karne ne hume pata lag raha hai agar tree mai gadbad hai
+    // o(n)
+    int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int left=height(root.left);
+        if(left==-1) return -1;
+
+        int right=height(root.right);
+        if(right==-1) return -1;
+
+        if(Math.abs(left-right)>1){
+            return -1;
+        }
+
+        return Math.max(left,right)+1;
+    }
+    public boolean isBalanced(TreeNode root) {
+        return height(root)!=-1;
     }
 }

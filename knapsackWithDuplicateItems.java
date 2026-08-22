5f6aaef426fb3f64d59a8ef466d81d0c78f910a3
itsmikhil <ailani.mikhil@gmail.com>
2026-08-22 14:05:53 +0530

    knapsackWithDuplicateItems

--- knapsackWithDuplicateItems.java
+++ knapsackWithDuplicateItems.java
@@ -135,3 +135,67 @@ class Solution {
         return prev[capacity];
     }
 }
+class Solution {
+
+    // Space Optimized Tabulation using prev and curr
+    // Infinite supply:
+    // TAKE -> same row
+    // NOT TAKE -> previous row
+    
+    // Why single array works?
+    //
+    // In 2D DP:
+    // dp[i][j] stores answer using items 0 to i with capacity j.
+    //
+    // For every state:
+    // notTake = dp[i-1][j]          -> previous row
+    // take    = val[i] + dp[i][j-wt[i]] -> same row
+    //
+    // We only need capacity values, not all rows.
+    //
+    // In 1D:
+    // - dp[j] stores the previous row's answer before updating.
+    //   So it works as notTake.
+    //
+    // - For take, we need dp[j-wt[i]] from the same row.
+    //   Because we move j from left to right, dp[j-wt[i]]
+    //   is already updated for the current item.
+    //
+    // Hence one array is enough.
+
+    // Time: O(n * capacity)
+    // Space: O(capacity)
+
+   public int knapSack(int val[], int wt[], int capacity) {
+
+   int dp[] = new int[capacity + 1];
+
+   // Only first item available
+   // Item can be picked multiple times
+   for(int cap = 0; cap <= capacity; cap++){
+
+       if(wt[0] <= cap){
+           dp[cap] = (cap / wt[0]) * val[0];
+       }
+   }
+
+
+   for(int i = 1; i < val.length; i++){
+
+       for(int j = 1; j <= capacity; j++){
+
+           int notTake = dp[j];
+
+           int take = Integer.MIN_VALUE;
+
+           if(wt[i] <= j){
+               take = val[i] + dp[j - wt[i]];
+           }
+
+           dp[j] = Math.max(take, notTake);
+       }
+   }
+
+   return dp[capacity];
+    }
+}

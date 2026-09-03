3b22eeb9288d05da5b763d5e7fc59cc4f0015e17
itsmikhil <ailani.mikhil@gmail.com>
2026-09-03 18:39:08 +0530

    LC88

---
+++ LC88.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // nums1 mai he place karo
+        // lekin place karna end se start karo
+        // so wahi hai merge sort wala 2 pointer approach
+        // simple
+        int i=m-1;
+        int j=n-1;
+        int k=(n+m)-1;
+
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+        while(i>=0){
+            nums1[k]=nums1[i];
+            i--;
+            k--;
+        }
+
+    }
+}
